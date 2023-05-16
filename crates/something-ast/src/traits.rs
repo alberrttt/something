c06a38dba602a39d6a8c5f9ba4360a0018eb83a6
Albert Hong <bertbocks@gmail.com>
2023-05-16 08:46:34 -0700

    try to work on errors

--- crates/something-ast/src/traits.rs
+++ crates/something-ast/src/traits.rs
@@ -3,3 +3,10 @@ use std::slice::Iter;
 pub trait Children<Child> {
     fn children(&self) -> Iter<Child>;
 }
+
+pub trait Name: std::fmt::Debug {
+    fn name() -> &'static str
+    where
+        Self: Sized;
+    fn named(&self) -> &'static str;
+}
